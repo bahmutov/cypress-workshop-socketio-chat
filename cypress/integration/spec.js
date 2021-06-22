cb5649c757cc9fa4a7da833c327e7b0d23feeb4d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-22 09:17:54 -0400

    add socket test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,17 +1,34 @@
 /// <reference types="cypress" />
 describe('Chat', () => {
   it('loads', () => {
+    const name = 'cy_' + Cypress._.random(10000)
     cy.visit('/', {
       onBeforeLoad(win) {
-        cy.stub(win, 'prompt').returns('Cy')
+        cy.stub(win, 'prompt').returns(name)
       },
     })
 
-    cy.contains('#messages li', 'Cy').should('be.visible')
+    cy.contains('#messages li', name).should('be.visible')
     cy.get('#txt').type('Hello there{enter}')
     cy.contains('#messages li', 'Hello there')
       .should('be.visible')
       .find('strong')
-      .should('have.text', 'Cy')
+      .should('have.text', name)
+
+    cy.task('connect', 'guest')
+    cy.contains('#messages li', 'guest join the chat..').should('exist')
+    cy.task('say', 'Nice to meet you')
+
+    cy.contains('#messages li', 'Nice to meet you')
+      .should('be.visible')
+      .find('strong')
+      .should('have.text', 'guest')
+
+    cy.get('#txt').type('Goodbye{enter}')
+    cy.contains('#messages li', 'Goodbye').should('be.visible')
+    cy.task('getLastMessage').should('include', 'Goodbye').and('include', name)
+
+    cy.task('disconnect')
+    cy.contains('#messages li', 'guest left the chat..').contains('🔴')
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,5 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const io = require('socket.io-client')
 
 /**
  * @type {Cypress.PluginConfig}
@@ -19,4 +8,35 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  let chatSocket
+  let lastMessage
+
+  on('task', {
+    connect(name) {
+      chatSocket = io('http://localhost:8080')
+      chatSocket.emit('username', name)
+      chatSocket.on('chat_message', (msg) => {
+        lastMessage = msg
+        console.log('lastMessage is "%s"', lastMessage)
+      })
+
+      return null
+    },
+
+    getLastMessage() {
+      return lastMessage || null
+    },
+
+    say(message) {
+      chatSocket.emit('chat_message', message)
+      return null
+    },
+
+    disconnect() {
+      chatSocket.disconnect()
+      chatSocket = null
+      return null
+    },
+  })
 }
